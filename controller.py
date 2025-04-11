e7f01e18c863f4ab92946c38c21fcfed465fe7c7
user65434680 <s2kava01@students.osao.fi>
2025-04-11 15:13:50 +0300

    commit31

--- controller.py
+++ controller.py
@@ -6,10 +6,7 @@ import json
 from random_gen import generate_random_password
 
 def run_ansible_playbook(playbook, ask_become_pass=False):
-    command = ['ansible-playbook', '-i', 'inventory.ini', playbook]
-    
-    if ask_become_pass:
-        command.append('--ask-become-pass')
+    command = ['ansible-playbook', '-i', 'inventory.ini', playbook, '--ask-become-pass']
     
     try:
         result = subprocess.run(command, check=True, text=True, capture_output=True)
