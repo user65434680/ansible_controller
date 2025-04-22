220f7d3b043feebf082bf33ecb3c0a0962392d57
user65434680 <s2kava01@students.osao.fi>
2025-04-22 09:53:13 +0300

    fixing return option...

--- run.py
+++ run.py
@@ -7,28 +7,29 @@ import sys
 
 def start():
     ensure_projects_folder()
-    print("\nWelcome to the system configuration tool.")
-    print("Please select an option from the menu.")
-    print("1. Create project")
-    print("2. Load project")
-    print("3. List projects")
-    print("4. Exit")
+    while True:
+        print("\nWelcome to the system configuration tool.")
+        print("Please select an option from the menu.")
+        print("1. Create project")
+        print("2. Load project")
+        print("3. List projects")
+        print("4. Exit")
 
-    choice = input("Choice: ").strip()
-    if choice == "1":
-        create_project()
-    elif choice == "2":
-        loaded_project = load_project()
-        if loaded_project:
-            print(f"Switching to main menu for project {loaded_project}...\n")
-            subprocess.run(["python3", "controller.py"])
-            sys.exit()
-    elif choice == "3":
-        list_projects()
-    elif choice == "4":
-        print("Exiting.")
-    else:
-        print("Invalid choice.")
+        choice = input("Choice: ").strip()
+        if choice == "1":
+            create_project()
+        elif choice == "2":
+            loaded_project = load_project()
+            if loaded_project:
+                print(f"Switching to main menu for project {loaded_project}...\n")
+                subprocess.run(["python3", "controller.py"])
+                sys.exit()
+        elif choice == "3":
+            list_projects()
+        elif choice == "4":
+            print("Exiting.")
+        else:
+            print("Invalid choice.")
 
 if __name__ == "__main__":
     start()
