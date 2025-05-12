e2c6bbb8cb4ddc59ce3f3a1d6e4d37301386bc10
user65434680 <s2kava01@students.osao.fi>
2025-05-12 13:55:00 +0300

    tried making a simple setup wizard

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from setup_wizard import wizard_menu
 from users_default.user_controller import user_control
 from unbound_default.domain_controller import choose_action
 from clients_default.client_control import client_control_menu
@@ -13,32 +14,35 @@ from push_changes import push_menu
 def main_menu():
     while True:
         print("Select an option:")
-        print("1. Users")
-        print("2. Client computers")
-        print("3. Domains")
-        print("4. Templates")
-        print("5. Push changes to client")
-        print("6. Return to projects")
-        print("7. Exit the system")
+        print("1. Easy setup wizard")
+        print("2. Users")
+        print("3. Client computers")
+        print("4. Domains")
+        print("5. Templates")
+        print("6. Push changes to client")
+        print("7. Return to projects")
+        print("8. Exit the system")
 
         choice = input("Enter the number of your choice: ").strip()
 
         if choice == "1":
-            user_control()
+            print("placeholder")
         elif choice == "2":
-            client_control_menu()
+            user_control()
         elif choice == "3":
-            choose_action()
+            client_control_menu()
         elif choice == "4":
-            template_controller_menu()
+            choose_action()
         elif choice == "5":
-            push_menu()
+            template_controller_menu()
         elif choice == "6":
+            push_menu()
+        elif choice == "7":
             print("Exiting to projects...")
             subprocess.run(["python3", "run.py"])
             sys.exit()
 
-        elif choice == "7":
+        elif choice == "8":
             exit_choice = input("If you exit some changes may not be saved. Are you sure you want to exit?\ntype: yes or no\n").strip()
                 
             if exit_choice == "yes":
---
+++ setup_wizard.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import os
+import json
+
+from users_default.user_controller import add_user
+from users_default.user_controller import add_users_from_file
+from clients_default.client_control import assign_computers_choice
+from clients_default.client_assingment import assign_computers_manually
+from clients_default.client_control import assign_computers_automatically
+from unbound_default.domain_controller import allow_domains
+
+
+print("Setup wizard is used to guide you through configuration in the right order.")
+input("Press 1 to start 2 to exit:").strip()
+if input == "1":
+     print("Starting setup wizard...")
+
+def wizard_menu():
+    while True:
+        if input == "1":
+            print("Starting setup wizard...")     
+        elif input == "2":
+            print("Exiting setup wizard.")
+            break
+        else:
+            print("Invalid input. Please enter 1 or 2.")
+
+def wizard():
+    while True:
+        print("Welcome to the wizard. You can exit at any time by typing 'exit'.")
+        
+        choice = input("Would you like to add new clients to ansible? This adds them to the control environment BUT these aren't the computers you will be pushing to.\n1. yes\n2. no\n").strip()
+        if choice == "exit":
+            break
+        elif choice == "1":
+            assign_computers_choice()
+        elif choice == "2":
+            pass
+        else:
+            print("Invalid input. Please enter 1 or 2.")
+            continue
+
+        choice = input("Please select how you would like to select clients you are going to push to.\n1. Manually\n2. Automatically\n3. skip (not recommended)\n").strip()
+        if choice == "exit":
+            break
+        elif choice == "1":
+            assign_computers_manually()
+        elif choice == "2":
+            assign_computers_automatically()
+        elif choice == "3":
+            print("Skipping...")
+        else:
+            print("Invalid input. Please enter 1, 2.or 3")
+            continue
+
+        choice = input("Please select how you would like to add users.\n1. Manually\n2. From a file\n3. skip\n").strip()
+        if choice == "exit":
+            break
+        elif choice == "1":
+            add_user()
+        elif choice == "2":
+            add_users_from_file()
+        elif choice == "3":
+            print("Skipping...")
+        else:
+            print("Invalid input. Please enter 1, 2 or 3.")
+            continue
+
+        choice = input("Would you like to add whitelisted websites?\n1. yes\n2. no\n").strip()
+        if choice == "exit":
+            break
+        elif choice == "1":
+            allow_domains()
+        elif choice == "2":
+            pass
+        else:
+            print("Invalid input. Please enter 1 or 2.")
+            continue
+
+        print("Setup wizard complete. You can now push changes to clients.")
+        break
