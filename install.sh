0a6325a0c8d0163d50d802c29d863de9ddca214b
user65434680 <s2kava01@students.osao.fi>
2025-05-19 09:58:28 +0300

    added install ssh pass

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ sudo apt-get install ansible -y
 
 ansible-galaxy collection install ansible.posix
 
+sudo apt install sshpass -y
+
 sudo apt install libssl-dev python3-dev build-essential -y
 
 sudo apt install python3-openssl -y
