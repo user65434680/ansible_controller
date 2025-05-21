48497e0d62d3624f283aeb9016d03554cd69be96
user65434680 <s2kava01@students.osao.fi>
2025-05-21 16:07:10 +0300

    added suricata install to the main script.

--- install.sh
+++ install.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+# Basic requirements installation
 sudo apt install python3 python3-pip -y
-
 sudo apt-get install ansible -y
-
 ansible-galaxy collection install ansible.posix
-
 sudo apt install sshpass -y
-
 sudo apt install libssl-dev python3-dev build-essential -y
-
 sudo apt install python3-openssl -y
+
+# Segment for optional scanning tools
+echo "Would you like to install Suricata and network scanning tools?"
+echo "This requires two network interfaces for the computer."
+read -p " (y/n): " choice
+if [[ "$choice" =~ ^[Yy]$ ]]; then
+
+    git clone https://github.com/user65434680/network_scanner network_scanner
+    cd network_scanner
+    chmod +x run_all.sh
+    sudo ./run_all.sh
+else
+    echo "Skipping Suricata and network scanning tools installation..."
+fi
+
+echo "Installation complete!"
