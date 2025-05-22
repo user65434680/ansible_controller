60848b4609fac66321f7f19458826421ece427e2
user65434680 <s2kava01@students.osao.fi>
2025-05-22 12:46:13 +0300

    changed again

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Basic requirements installation
 sudo apt install python3 python3-pip -y
 sudo apt-get install ansible -y
 ansible-galaxy collection install ansible.posix
@@ -10,30 +9,31 @@ sudo apt install python3-openssl -y
 
 sudo ../ansible_project_icon/setup_ansible_icon.sh
 
-# Segment for optional scanning tools
 echo "Would you like to install Suricata and network scanning tools?"
 echo "This requires two network interfaces for the computer."
 read -p " (y/n): " choice
-if [[ "$choice" =~ ^[Yy]$ ]]; then
 
+if [[ "$choice" =~ ^[Yy]$ ]]; then
     git clone https://github.com/user65434680/network_scanner network_scanner
     cd network_scanner
     chmod +x run_all.sh
     sudo ./run_all.sh
-    echo "[!!] Script will be copied to /opt/ansible_controller [!!]"
-    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    DEST="/opt/ansible_controller"
-
-    sudo mkdir -p "$DEST"
-    sudo cp -r "$SCRIPT_DIR"/* "$DEST"
 else
     echo "Skipping Suricata and network scanning tools installation..."
     echo "[*] Scheduling system reboot in 10 seconds..."
     sudo shutdown -r +1 "System will reboot in 10 seconds"
-    echo "[!!] Script will be copied to /opt/ansible_controller [!!]"
-    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    DEST="/opt/ansible_controller"
+fi
 
-    sudo mkdir -p "$DEST"
-    sudo cp -r "$SCRIPT_DIR"/* "$DEST"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DEST="/opt/ansible_controller"
+
+echo "[!!] Copying script to $DEST... [!!]"
+sudo mkdir -p "$DEST"
+sudo cp -r "$SCRIPT_DIR"/* "$DEST"
+
+if [[ -f "$DEST/$(basename "$0")" ]]; then
+    echo "Copy successful. Cleaning up source directory..."
+    sudo rm -rf "$SCRIPT_DIR"
+else
+    echo "Copy failed. Skipping deletion of original directory."
 fi
