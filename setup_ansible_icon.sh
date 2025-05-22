807e20546d23bc24a7c11d2f73cfd112486dd294
user65434680 <s2kava01@students.osao.fi>
2025-05-22 14:26:20 +0300

    remade setup code

--- setup_ansible_icon.sh
+++ setup_ansible_icon.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# setup network scanner for controller
 SCRIPT_PATH="/opt/ansible_icon/ansible_icon.py"
 GENERAL_PATH="/opt/ansible_icon/start.sh"
 SERVICE_PATH="/etc/systemd/system/ansible_icon.service"
@@ -9,17 +8,19 @@ if [[ "$EUID" -ne 0 ]]; then
   echo "Please run this script as root or with sudo."
   exit 1
 fi
+
+
 mkdir -p /opt/ansible_icon
 sudo cp ansible_project_icon/ansible_icon.py "$SCRIPT_PATH"
 sudo cp ansible_project_icon/start.sh "$GENERAL_PATH"
 
-if [[ -f "$SCRIPT_PATH" ]]; then
-  chmod +x "$SCRIPT_PATH"
-  echo "Made $SCRIPT_PATH executable."
-else
-  echo "Error: Script $SCRIPT_PATH does not exist."
-  exit 1
-fi
+
+sudo chmod 755 /opt/ansible_icon
+sudo chmod 755 "$SCRIPT_PATH"
+sudo chmod 755 "$GENERAL_PATH"
+sudo chown root:root "$SCRIPT_PATH"
+sudo chown root:root "$GENERAL_PATH"
+
 
 cat <<EOF > "$SERVICE_PATH"
 [Unit]
@@ -29,24 +30,14 @@ Wants=network-online.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/python3 /opt/ansible_icon/ansible_icon.py
+ExecStart=/usr/bin/python3 $SCRIPT_PATH
 RemainAfterExit=yes
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-echo "Created systemd service file at $SERVICE_PATH."
 
-
-systemctl daemon-reexec
 systemctl daemon-reload
-systemctl enable ansible_icon.service"
-
-echo "Service enabled to run on boot: ansible_icon.service""
-
-echo "Setting permissions..."
-sudo chmod 700 /opt/ansible_icon
-sudo chmod 700 /opt/ansible_controller/ansible_icon.py
-sudo chown root:root /opt/ansible_controller/ansible_icon.py
-sudo chmod +x /opt/ansible_icon/start.sh
+systemctl enable ansible_icon.service
+echo "Service enabled to run on boot: ansible_icon.service"
